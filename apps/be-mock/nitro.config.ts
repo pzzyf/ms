4c2e764eb55d2fa9ec60045a653dea407bc61857
pzzyf <2279948211@qq.com>
2026-06-29 17:22:37 +0800

    chore: /auth/login

--- apps/be-mock/nitro.config.ts
+++ apps/be-mock/nitro.config.ts
@@ -1,8 +1,15 @@
-import { defineNitroConfig } from 'nitropack/config'
-
-// https://nitro.build/config
 export default defineNitroConfig({
-  compatibilityDate: 'latest',
-  srcDir: 'server',
-  imports: false,
+  routeRules: {
+    '/api/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers':
+          'Accept, Authorization, Content-Length, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
+        'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Expose-Headers': '*',
+      },
+    },
+  },
 })
